7f90a657aeef7be2264f0a7930aafd2ac0281502
Pavel <Chesh397@mail.ru>
2019-01-24 00:48:18 +0300

    Add Byte-String convervion

--- MicroServer/MicroServer.js
+++ MicroServer/MicroServer.js
@@ -4,10 +4,11 @@ const fs = require("fs");
 const DEFAULT_PORT = 10391;
 
 // Expected at least 1 arg (or 2)
-const serverAddress = process.env.SERVER;
+const serverAddress = "http://0.0.0.0:8081";
 
 if (!serverAddress) {
 
+    console.log("Error: Server is not available.");
     return -1;
 
 }
@@ -18,22 +19,38 @@ const socket = socketIOClient(serverAddress + "/sandbox");
 
 setTimeout(() => exit = true, 10 * 60 * 1000);
 
+function uintToString(uintArray) {
+    var encodedString = String.fromCharCode.apply(null, uintArray),
+        decodedString = decodeURIComponent(escape(encodedString));
+    return decodedString;
+}
+
+function stringToUint(string) {
+    var string = btoa(unescape(encodeURIComponent(string))),
+        charList = string.split(''),
+        uintArray = [];
+    for (var i = 0; i < charList.length; i++) {
+        uintArray.push(charList[i].charCodeAt(0));
+    }
+    return new Uint8Array(uintArray);
+}
+
 socket.on("exec", function(data) {
 
     let dotnet = spawn("dotnet", ["run", "-p", "ConsoleApplication"]);
     dotnet.stdout.on("data", function(data) {
 
-        socket.emit("o", { "output": data });
+        socket.emit("o", { "output": uintToString(data) });
         
     });
     dotnet.stderr.on("data", function(data) {
 
-        socket.emit("e", { "error": data });
+        socket.emit("e", { "error": uintToString(data) });
 
     });
     let inputHandler = function(data) {
 
-        dotnet.stdin.pipe(data.input);
+        dotnet.stdin.pipe(stringToUint(data.input));
             
     }
     socket.on("i", inputHandler);
@@ -47,5 +64,3 @@ socket.on("exec", function(data) {
     });
 
 });
-
-while (!exit);
